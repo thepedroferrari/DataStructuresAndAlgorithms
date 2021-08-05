dc7c8158af3fe5ff64c4076211da672a3540f9cd
thepedroferrari <pedro@pedroferrari.com>
2021-08-05 23:09:16 +0200

    chore: Add set of simple steps to arrive into the solution

--- Project 1/P0/Task4.py
+++ Project 1/P0/Task4.py
@@ -25,3 +25,30 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+# Set of possible telemarketers
+# Numbers make outgoing calls but do not receive Texts or calls or send texts. In other words,
+# some numbers might receive only one of these 3 and would not be considered potential telemarketers.
+
+
+# Simple hand solution:
+
+# A List of all numbers that make calls.
+# B List of all numbers that receive calls
+# C List of all numbers that receive texts
+# D List of all numbers that send texts
+
+# To be a possible telemarketer, you must be in A, but not B, C, or D.
+
+# Make the lists into a set. In this case if only one of either is met is enough, we don't care for duplicates.
+# Loop through A.
+# Check if each A is contained in either B, C or D
+# If TRUE: Delete the number from the Set
+# If FALSE: do nothing and continue.
+# We are looking at a quadratic algorithm here.
+# When done, sort the Set. sort(set) should be O(n log n)
+
+# Print message
+# Loop through A and print each of the remaining numbers one by one.
+
+
+
